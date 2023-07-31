c21ff7075a3bbbbfe853b1d9c5900c48f06b57d6
manish8094 <manishparashar809495@gmail.com>
2023-07-31 18:20:23 +0530

    make code easy and understand

--- src/String/StringPractice2.java
+++ src/String/StringPractice2.java
@@ -3,60 +3,31 @@ package String;
 import java.util.Arrays;
 
 public class StringPractice2 {
+	public static boolean areAnagrams(String str1, String str2) {
+		// Remove spaces and convert to lowercase
+		String s1 = str1.replaceAll("\\s", "").toLowerCase();
+		String s2 = str2.replaceAll("\\s", "").toLowerCase();
 
-	/*
-	 * One way to find if two Strings are anagram in Java. This method assumes both
-	 * arguments are not null and in lowercase.
-	 *
-	 * @return true, if both String are anagram
-	 */
-	public static boolean isAnagram(String word, String anagram) {
-		if (word.length() != anagram.length()) {
-			return false;
-		}
+		// Convert to character arrays
+		char[] arr1 = s1.toCharArray();
+		char[] arr2 = s2.toCharArray();
 
-		char[] chars = word.toCharArray();
+		// Sort the arrays
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
 
-		for (char c : chars) {
-			int index = anagram.indexOf(c);
-			if (index != -1) {
-				anagram = anagram.substring(0, index) + anagram.substring(index + 1, anagram.length());
-			} else {
-				return false;
-			}
-		}
-
-		return anagram.isEmpty();
+		// Compare the sorted arrays
+		return Arrays.equals(arr1, arr2);
 	}
 
-	/*
-	 * Another way to check if two Strings are anagram or not in Java This method
-	 * assumes that both word and anagram are not null and lowercase
-	 * 
-	 * @return true, if both Strings are anagram.
-	 */
-	public static boolean iAnagram(String word, String anagram) {
-		char[] charFromWord = word.toCharArray();
-		char[] charFromAnagram = anagram.toCharArray();
-		Arrays.sort(charFromWord);
-		Arrays.sort(charFromAnagram);
+	public static void main(String[] args) {
+		String str1 = "listen";
+		String str2 = "silent";
 
-		return Arrays.equals(charFromWord, charFromAnagram);
-	}
-
-	public static boolean checkAnagram(String first, String second) {
-		char[] characters = first.toCharArray();
-		StringBuilder sbSecond = new StringBuilder(second);
-
-		for (char ch : characters) {
-			int index = sbSecond.indexOf("" + ch);
-			if (index != -1) {
-				sbSecond.deleteCharAt(index);
-			} else {
-				return false;
-			}
+		if (areAnagrams(str1, str2)) {
+			System.out.println("The strings are anagrams.");
+		} else {
+			System.out.println("The strings are not anagrams.");
 		}
-
-		return sbSecond.length() == 0 ? true : false;
 	}
 }
