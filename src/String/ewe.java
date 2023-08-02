b17c2d307843deb6ea03c416ed25395f75fc9852
Abdul Rehman Khan <82725246+khaabdul@users.noreply.github.com>
2023-08-02 20:35:13 +0530

    Update ewe.java
    added code

--- src/String/ewe.java
+++ src/String/ewe.java
@@ -1,5 +1,34 @@
-package String;
+package string;
+
+import java.util.Arrays;
 
 public class ewe {
+	static int i, j, count = 0, temp = 0;
+	static String val = " ", tem = " ";
+	static int o[];
+	static int p[];
+
+	public static void main(String[] args) {
+		int a[] = { 1020304050 };
+		String s = Integer.toString(a[0]);
+		String[] split = s.split("");
+		int[] values = new int[split.length];
+
+		for (int i = 0; i < split.length; i++) {
+			values[i] = Integer.parseInt(split[i]);
+		}
+		for (int z = 0; z < values.length; z++) {
+
+			if (values[z] == 0) {
+				val = values[z] + val;
+			} else if (values[z] != 0) {
+				tem = values[z] + tem;
+			}
+		}
+		StringBuffer sb=new StringBuffer(tem);
+		sb.reverse();
+		System.out.println(sb+val);
+	}
 
 }
+
