d3e2c0feb4f6f7845c6ec342af266232003ad2fd
Manish parashar <121606120+ManishParashar0@users.noreply.github.com>
2023-08-16 19:02:10 +0530

    practice

---
+++ src/Pattern/Pattern1.java
@@ -0,0 +1,19 @@
+package Pattern;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 7; i++) {
+
+			for (int j = 0; j < 7; j++) {
+				if (i == 5 && j == 5) {
+					System.out.println("+");
+				}
+				if (i == 1 && j == 1) {
+
+				}
+
+			}
+		}
+	}
+
+}
