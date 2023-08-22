307b31efdbd55eeaa6741d15a87db85341f9d290
Manish parashar <121606120+ManishParashar0@users.noreply.github.com>
2023-08-22 18:32:19 +0530

    some pattern add

--- src/Pattern/m.java
+++ src/Pattern/m.java
@@ -1,18 +1,9 @@
 package Pattern;
 
-class test12 {
-
-}
-
-class c extends test12 {
-
-}
-
 class m {
 
 	public static void main(String args[]) {
-		test12 a = new c();
-		System.out.println("hello");
+
 	}
 
 }
